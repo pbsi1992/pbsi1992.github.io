1c9bbdad29f7de13b14554ffa16e1fcb4a1fca26
Jero Halili <127023985+JeroH20@users.noreply.github.com>
2024-10-06 13:57:56 +0800

    first commit

---
+++ js/!!!script.js
@@ -0,0 +1,5 @@
+// document.getElementById('testApplyForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     alert('Form submitted successfully! Redirecting to sent.html...');
+//     window.location.href = 'sent.html';
+// });
