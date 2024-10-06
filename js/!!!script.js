4737cc42df714531dcdc862515c82cea6f3533d8
Jero Halili <127023985+JeroH20@users.noreply.github.com>
2024-10-06 14:36:48 +0800

    fix1 commit

--- js/!!!script.js
+++
@@ -1,5 +0,0 @@
-// document.getElementById('testApplyForm').addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     alert('Form submitted successfully! Redirecting to sent.html...');
-//     window.location.href = 'sent.html';
-// });
